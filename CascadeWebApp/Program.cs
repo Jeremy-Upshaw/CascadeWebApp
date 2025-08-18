927a60132b850b8c0e36a536017251aae4a24c16
Jeremy-Upshaw <jeremy@cascadebodyjewelry.com>
2025-08-18 12:29:42 -0700

    sync all project files

--- CascadeWebApp/Program.cs
+++ CascadeWebApp/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using MudBlazor.Services;
 using CascadeWebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddMudServices();
 
 // Register your custom services
 builder.Services.AddScoped<ItemService>();
