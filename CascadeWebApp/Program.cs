8dd7b3a41c70e9a12b99730d6511f8433f1a807b
Jeremy-Upshaw <jeremy@cascadebodyjewelry.com>
2025-08-17 17:33:59 -0700

    Move code folders into project folder for correct structure

--- CascadeWebApp/Program.cs
+++ CascadeWebApp/Program.cs
@@ -1,27 +1,35 @@
-using CascadeWebApp.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
+using CascadeWebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
+
+// Register your custom services
+builder.Services.AddScoped<ItemService>();
+builder.Services.AddScoped<OrderService>();
+builder.Services.AddScoped<CustomerService>();
+builder.Services.AddScoped<BatchService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-app.UseAntiforgery();
+app.UseRouting();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.Run();
