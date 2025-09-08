08a27fe1ef95b21489ba5bbe7d716027b407431a
Jeremy-Upshaw <jeremy@cascadebodyjewelry.com>
2025-09-08 11:27:53 -0700

    Update Customer.cs

--- CascadeWebApp/Models/Customer.cs
+++ CascadeWebApp/Models/Customer.cs
@@ -13,20 +13,18 @@ namespace CascadeWebApp.Models
         public string Phone { get; set; } = string.Empty;
         public string DiscountLevel { get; set; } = string.Empty;
         public string PaymentTerms { get; set; } = string.Empty;
-        public string ContactFirstName { get; set; } = string.Empty;
-        public string ContactSecondName { get; set; } = string.Empty;
-        public string ShippingAddressLine1 { get; set; } = string.Empty;
-        public string ShippingAddressLine2 { get; set; } = string.Empty;
-        public string ShippingCity { get; set; } = string.Empty;
-        public string ShippingState { get; set; } = string.Empty;
-        public string ShippingZip { get; set; } = string.Empty;
-        public string ShippingCountry { get; set; } = string.Empty;
-        public string BillingAddressLine1 { get; set; } = string.Empty;
-        public string BillingAddressLine2 { get; set; } = string.Empty;
-        public string BillingCity { get; set; } = string.Empty;
-        public string BillingState { get; set; } = string.Empty;
-        public string BillingZip { get; set; } = string.Empty;
-        public string BillingCountry { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Ship_To_1 { get; set; } = string.Empty;
+        public string Ship_To_2 { get; set; } = string.Empty;
+        public string Ship_To_3 { get; set; } = string.Empty;
+        public string Ship_To_4 { get; set; } = string.Empty;
+        public string Ship_To_5 { get; set; } = string.Empty;
+        public string Bill_To_1 { get; set; } = string.Empty;
+        public string Bill_To_2 { get; set; } = string.Empty;
+        public string Bill_To_3 { get; set; } = string.Empty;
+        public string Bill_To_4 { get; set; } = string.Empty;
+        public string Bill_To_5 { get; set; } = string.Empty;
 
         // Navigation properties
         public virtual ICollection<OrdersList> Orders { get; set; } = new List<OrdersList>();
