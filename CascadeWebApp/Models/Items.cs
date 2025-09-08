f82a0e04540d28cadc09f04c6e96e0c4b5b4fa80
Jeremy-Upshaw <jeremy@cascadebodyjewelry.com>
2025-09-08 07:12:17 -0700

    Change item properties to nullable doubles

--- CascadeWebApp/Models/Items.cs
+++ CascadeWebApp/Models/Items.cs
@@ -15,16 +15,16 @@ namespace CascadeWebApp.Models
         public string EndMM { get; set; } = string.Empty;
         public string EndInch { get; set; } = string.Empty;
         public string Length { get; set; } = string.Empty;
-        public double OnOrder { get; set; }
-        public double InProduction { get; set; }
-        public double OnTheWall { get; set; }
-        public double InTheShop { get; set; }
-        public double Available { get; set; }
+        public double? OnOrder { get; set; }
+        public double? InProduction { get; set; }
+        public double? OnTheWall { get; set; }
+        public double? InTheShop { get; set; }
+        public double? Available { get; set; }
         public double? MachiningLength { get; set; }
         public double? CycleTime { get; set; }
-        public double StockQty { get; set; }
-        public double BuildQty { get; set; }
-        public double MinStockSize { get; set; }
+        public double? StockQty { get; set; }
+        public double? BuildQty { get; set; }
+        public double? MinStockSize { get; set; }
         public virtual ICollection<OrderContents> OrderContents { get; set; } = new List<OrderContents>();
         public virtual ICollection<BatchContents> BatchContents { get; set; } = new List<BatchContents>();
     }
